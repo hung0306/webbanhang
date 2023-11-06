b13ab2d1f5e36dbb11994c7c864cb997c18db7b8
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-11-06 22:15:43 +0700

    1.3. Hiển thị A vào danh sách Lời mời kết bạn của B

--- public/js/users.js
+++ public/js/users.js
@@ -33,34 +33,40 @@ if(listBtnCancelFriend.length > 0) {
 
 
 // Chức năng từ chối kết bạn
+const refuseFriend = (button) => {
+  button.addEventListener("click", () => {
+    button.closest(".box-user").classList.add("refuse");
+
+    const userId = button.getAttribute("btn-refuse-friend");
+
+    socket.emit("CLIENT_REFUSE_FRIEND", userId);
+  });
+};
+
 const listBtnRefuseFriend = document.querySelectorAll("[btn-refuse-friend]");
 if(listBtnRefuseFriend.length > 0) {
   listBtnRefuseFriend.forEach(button => {
-    button.addEventListener("click", () => {
-      button.closest(".box-user").classList.add("refuse");
-
-      const userId = button.getAttribute("btn-refuse-friend");
-      // console.log(userId);
-
-      socket.emit("CLIENT_REFUSE_FRIEND", userId);
-    });
+    refuseFriend(button);
   });
 }
 // Hết Chức năng từ chối kết bạn
 
 
 // Chức năng chấp nhận kết bạn
+const acceptFriend = (button) => {
+  button.addEventListener("click", () => {
+    button.closest(".box-user").classList.add("accepted");
+
+    const userId = button.getAttribute("btn-accept-friend");
+
+    socket.emit("CLIENT_ACCEPT_FRIEND", userId);
+  });
+}
+
 const listBtnAcceptFriend = document.querySelectorAll("[btn-accept-friend]");
 if(listBtnAcceptFriend.length > 0) {
   listBtnAcceptFriend.forEach(button => {
-    button.addEventListener("click", () => {
-      button.closest(".box-user").classList.add("accepted");
-
-      const userId = button.getAttribute("btn-accept-friend");
-      // console.log(userId);
-
-      socket.emit("CLIENT_ACCEPT_FRIEND", userId);
-    });
+    acceptFriend(button);
   });
 }
 // Hết Chức năng chấp nhận kết bạn
@@ -76,3 +82,71 @@ if(badgeUsersAccept) {
   });
 }
 // End SERVER_RETURN_LENGTH_ACCEPT_FRIEND
+
+// SERVER_RETURN_INFO_ACCEPT_FRIEND
+const dataUsersAccept = document.querySelector("[data-users-accept]");
+if(dataUsersAccept) {
+  const userId = dataUsersAccept.getAttribute("data-users-accept");
+  socket.on("SERVER_RETURN_INFO_ACCEPT_FRIEND", (data) => {
+    if(userId === data.userId) {
+      // Vẽ user ra giao diện
+      const div = document.createElement("div");
+      div.classList.add("col-6");
+
+      div.innerHTML = `
+        <div class="box-user">
+          <div class="inner-avatar">
+            <img src="/images/avatar.png" alt="${data.infoUserA.fullName}">
+          </div>
+          <div class="inner-info">
+            <div class="inner-name">
+            ${data.infoUserA.fullName}
+            </div>
+            <div class="inner-buttons">
+              <button
+                class="btn btn-sm btn-primary mr-1"
+                btn-accept-friend="${data.infoUserA._id}"
+              >
+                Chấp nhận
+              </button>
+              <button
+                class="btn btn-sm btn-secondary mr-1"
+                btn-refuse-friend="${data.infoUserA._id}"
+              >
+                Xóa
+              </button>
+              <button
+                class="btn btn-sm btn-secondary mr-1"
+                btn-deleted-friend=""
+                disabled=""
+              >
+                Đã xóa
+              </button>
+              <button
+                class="btn btn-sm btn-primary mr-1"
+                btn-accepted-friend=""
+                disabled=""
+              >
+                Đã chấp nhận
+              </button>
+            </div>
+          </div>
+        </div>
+      `;
+
+      dataUsersAccept.appendChild(div);
+      // Hết Vẽ user ra giao diện
+
+      // Hủy lời mời kết bạn
+      const buttonRefuse = div.querySelector("[btn-refuse-friend]");
+      refuseFriend(buttonRefuse);
+      // Hết Hủy lời mời kết bạn
+
+      // Chấp nhận lời mời kết bạn
+      const buttonAccept = div.querySelector("[btn-accept-friend]");
+      acceptFriend(buttonAccept);
+      // Hết Chấp nhận lời mời kết bạn
+    }
+  });
+}
+// End SERVER_RETURN_INFO_ACCEPT_FRIEND
